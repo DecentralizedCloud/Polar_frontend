720e1cbd0b73bc0711d604fdad1703fac8acc57f
siddheshkotkar <kotkar.sidh@gmail.com>
2022-04-18 13:03:25 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-
+import { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+	useEffect(()=>{
+		console.clear();
+
+const app = (() => {
+  let body;
+  let menu;
+  let menuItems;
+
+  const init = () => {
+    body = document.querySelector('body');
+    menu = document.querySelector('.menu-icon');
+    menuItems = document.querySelectorAll('.nav__list-item');
+
+    applyListeners();
+  };
+
+  const applyListeners = () => {
+    menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+  };
+
+  const toggleClass = (element, stringClass) => {
+    if (element.classList.contains(stringClass))
+    element.classList.remove(stringClass);else
+
+    element.classList.add(stringClass);
+  };
+
+  init();
+})();
+	},[]);
+
   return (
     <div className="App">
-      <div class="menu-icon">
+      <div className="menu-icon">
         
-	<span class="menu-icon__line menu-icon__line-left"></span>
-	<span class="menu-icon__line"></span>
-	<span class="menu-icon__line menu-icon__line-right"></span>
+	<span className="menu-icon__line menu-icon__line-left"></span>
+	<span className="menu-icon__line"></span>
+	<span className="menu-icon__line menu-icon__line-right"></span>
 </div>
 
-<div class="nav">
+<div className="nav">
 
-	<div class="nav__content">
-		<ul class="nav__list">
-			<li class="nav__list-item">Home</li>
-			<li class="nav__list-item">About</li>
-			<li class="nav__list-item">Projects</li>
-			<li class="nav__list-item">Contact</li>
+	<div className="nav__content">
+		<ul className="nav__list">
+			<li className="nav__list-item">Home</li>
+			<li className="nav__list-item">Resources</li>
+			<li className="nav__list-item">About us</li>
 		</ul>
 	</div>
 </div>
 
-<div class="site-content">
-	<h1 class="site-content__headline">Polar S2</h1>
+<div className="site-content">
+	<h1 className="site-content__headline">Polar S2</h1>
 </div>
 
     </div>
   );
 }
+
 
 export default App;
